d86bb72a1e1cd8b34d02de9af9bbe5ece71dbf83
4LEAF.YSH <4leaf.ysh@gmail.com>
2021-03-18 16:51:18 +0900

    a

---
+++ graphql/models/Store.js
@@ -0,0 +1,41 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+
+const Store = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    address: {
+      type: String,
+      required: true,
+    },
+    lnt: {
+      type: String,
+      required: true,
+    },
+    att: {
+      type: String,
+      required: true,
+    },
+    thumbnailPath: {
+      type: String,
+      required: true,
+    },
+    tel: {
+      type: String,
+      required: true,
+    },
+    workTime: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    versionKey: false,
+  }
+);
+
+export default mongoose.model(`Store`, Store, `Store`);
