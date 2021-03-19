32857e4bb5fa45a2fe6727c34d41a403a0e48338
4LEAF.YSH <4leaf.ysh@gmail.com>
2021-03-19 16:06:27 +0900

    a

--- graphql/api/Store/store/store.js
+++ graphql/api/Store/store/store.js
@@ -2,6 +2,18 @@ import Store from "../../../models/Store";
 
 export default {
   Query: {
+    getStoreOne: async (_, args) => {
+      const { id } = args;
+
+      try {
+        const result = await Store.findOne({ _id: id });
+
+        return result;
+      } catch (e) {
+        console.log(e);
+        return {};
+      }
+    },
     getAllStore: async (_, args) => {
       try {
         const result = await Store.find().sort({ title: 1 });
